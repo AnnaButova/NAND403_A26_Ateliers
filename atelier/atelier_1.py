5e447639560d7aafa02ff27599ba8d513aad23f5
Anna Butova <anya.butova2017@gmail.com>
2026-09-21 16:54:46 -0400

    push atelier 1

---
+++ atelier/atelier_1.py
@@ -0,0 +1,11 @@
+# Demander d'entrer le nom
+print("Qui etes-vous?")
+
+# Lire la reponse
+name = input()
+
+# Greetings
+print(f"Bienvenue a Cactusity {name}!")
+
+ville = input("D'ou viens tu?")
+print('cool "bled" ' + f"{ville}" + "!... '\n'Dude'")
